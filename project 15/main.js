26461c91fb2dbb2a36067ad0d975220347dde93f
Abdellah <AbdellahKarani>
2024-05-11 19:03:47 +0100

    .

--- project 15/main.js
+++ project 15/main.js
@@ -1,5 +1,3 @@
-// dO THE LOCAL STORAGE 
-
 // start getting variables
 var submit_edit = document.querySelector(".submit_edit");
 var add_task = document.querySelector(".add_task");
@@ -11,11 +9,10 @@ var elements = [];
 // localStorage.setItem('myArray', JSON.stringify([1, 2, 3, 4, 5]));
 // event.target.matches(".submit_edit");
 
-var elements_local = localStorage.getItem('myArray', JSON.stringify(elements));
-if (elements_local) {
-    elements_local = JSON.parse(elements_local);
-    elements_local.forEach(text => create_ele(text));
-    console.log(elements);
+var local_ele = localStorage.getItem('myArray', JSON.stringify(elements));
+if (local_ele) {
+    local_ele = JSON.parse(local_ele);
+    local_ele.forEach(text => create_ele(text));
 }
 
 
@@ -34,8 +31,8 @@ function create_ele(text) {
     icon1.className = "fa-solid fa-pen-to-square edit-icon";
     icon2.className = "fa-solid fa-trash remove-icon";
     // Styling && Text Content
-    task.style.cssText = "padding: 10px; border-radius: 5px; background-color: #eee; margin-bottom: 10px;\
-    display: flex; justify-content: space-between;";
+    task.style.cssText = "padding: 10px; border-radius: 5px; background-color: white; margin-bottom: 10px;\
+    display: flex; justify-content: space-between; border: 2px solid #641cff; text-align: left;";
     span.style.cssText = "font-size: 18px;";
     icons.style.cssText = "text-align: right; cursor: pointer;";
     icon1.style.cssText = "color: green; cursor: pointer;";
@@ -81,6 +78,11 @@ submit_edit.addEventListener("click", function(e){
     }
     if (submit_edit.value == "Edit" && add_task.value) {
         const must_edit = document.querySelector(".must_edit");
+        // Edit From Local Storage
+        const index_of_edit_ele = elements.indexOf(must_edit.textContent);
+        elements[index_of_edit_ele] = add_task.value;
+        localStorage.setItem('myArray', JSON.stringify(elements));
+        // Edit The Content + Some Changes
         must_edit.textContent = add_task.value;
         add_task.value = "";
         must_edit.classList.remove("must_edit");
@@ -94,8 +96,8 @@ submit_edit.addEventListener("click", function(e){
 document.addEventListener("click", function(event) {
     if (event.target.matches(".edit-icon")) {
         const matches_task_edit = event.target.parentElement.parentElement.firstChild;
-        // Some Changes
-        event.target.parentElement.parentElement.firstChild.classList.add("must_edit");
+        // Edit From Task + Some Changes
+        matches_task_edit.classList.add("must_edit");
         add_task.value = matches_task_edit.textContent;
         submit_edit.value = "Edit";
     }
@@ -104,7 +106,13 @@ document.addEventListener("click", function(event) {
 // Remove Element
 document.addEventListener("click", function(event) {
     if (event.target.matches(".remove-icon")) {
+        // Remove Elements From elements and set The Local Storage
         const matches_task_remove = event.target.parentElement.parentElement;
+        const index_ele = elements.indexOf(matches_task_remove.firstChild.textContent);
+        elements.splice(index_ele, 1);
+        localStorage.setItem('myArray', JSON.stringify(elements));
+        console.log(elements);
+        // Remove Elements
         matches_task_remove.remove();
         alert_ele("Item Removed", "red", "show");
         // Some Changes About Remove Button Of Clear List
@@ -120,6 +128,9 @@ clear_btn.addEventListener("click", function() {
     tasks_eles.forEach(element => element.remove());
     clear_btn.classList.remove("show");
     alert_ele("Empty List", "show", "red");
+    // Set The Locat Storage
+    elements = [];
+    localStorage.setItem('myArray', JSON.stringify(elements));
 });
 
 
