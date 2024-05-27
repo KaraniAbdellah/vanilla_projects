a115c67a0e6c546a64d2eea144059ef409ecf451
Abdellah <AbdellahKarani>
2024-05-27 15:20:50 +0100

    .

--- project 16/main.js
+++ project 16/main.js
@@ -1,39 +1,42 @@
 // Getting The Variables
-var slide_container = document.querySelector("slide-conatiner");
 var next = document.querySelector(".next");
 var prev = document.querySelector(".prev");
-var slide_img = document.querySelector(".slide img");
-var myCounter = document.querySelector(".counter");
 var images = document.querySelectorAll(".slide");
 var count = 1;
 
-for (var i = 0; i < images.length; i++) {
-    images[i].style.cssText = `left:${i}00%;`;
-    images[i].style.cssText = `transform: translateX(${i}00%);`;
-}
+window.addEventListener("DOMContentLoaded", function() {
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.cssText = `left:${i}00%;`;
+        images[i].style.cssText = `transform: translateX(${i}00%);`;
+    }
+    images.forEach(ele => ele.style.cssText = `transform: translateX(-100%);`); 
+});
 
-function show(count) {
-    images[count].style.cssText = `left:${-count}%;`;
-    images[count].style.cssText = `transform: translateX(-${-count}%);`;
-}
+
  
-
+function next_set(count) {
+    images[count].style.cssText = `transform: translateX(0);`;
+    images[count - 1].style.cssText = `transform: translateX(-100%);`; 
+}
+function prev_set(count) {
+    images[count - 1].style.cssText = `transform: translateX(0);`;
+    images[count].style.cssText = `transform: translateX(-100%);`;  
+}
 
 // check something
 next.addEventListener("click", function() {
-    console.log(images[count]);
     // Start Fix The Show And Hidden Elements
-    show(count);
+    next_set(count);
     // Start Fix Counter
-    myCounter.textContent = count + 1;
     prev.classList.remove("hidden");
     count++;
     if (count == images.length) next.classList.add("hidden");
 });
 prev.addEventListener("click", function() {
     count--;
-    console.log(images[count - 1]);
-    myCounter.textContent = count;
+    // Start Fix The Show And Hidden Elements
+    prev_set(count);
+    // Start Fix Counter
     if (count != images.length) next.classList.remove("hidden");
     if (count == 1) prev.classList.add("hidden");
 });
