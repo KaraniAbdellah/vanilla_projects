5621d9cd146a627b233a16f40756d29f3aa921e7
Abdellah <AbdellahKarani>
2024-06-19 20:44:49 +0100

    .

--- project 17/main.js
+++ project 17/main.js
@@ -1,193 +1,290 @@
-// You Task Is
-/**
- * Custom A Lot Of Things
- * Modifier The Players [Player1 --> x] && [Player2 --> 0]
- * Use The Local Storage For The Result Of The Players
- * Custom The Matrix Conditions
- */
+// Getting The Varibles
+const cells = document.querySelectorAll(".cell");
+const reset_btn = document.querySelector(".reset");
+var turn = true;
+var usedCells = [];
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
-
-
-
-// Getting The Varibales
-var x_symbol = document.querySelector(".x");
-var o_symbol = document.querySelector(".o");
-var current_trun = document.querySelector(".turn .current-trun");
-var reset_btn = document.querySelector(".reset");
-var celles = document.querySelectorAll(".cell");
-var player1 = document.querySelector(".player1 span");
-var player2 = document.querySelector(".player2 span");
-var draws = document.querySelector(".draws span");
-var toolge = document.querySelector(".toogle");
-var mark = document.querySelector(".toogle .mark");
-var winner = document.querySelector(".winner");
-var content = document.querySelector(".content");
-var i = 0;
-
-// Set The Winner And Loser Results
-if (sessionStorage.getItem("player1") && sessionStorage.getItem("player2") && sessionStorage.getItem("draws")) {
-    player1.textContent = sessionStorage.getItem("player1");
-    player2.textContent = sessionStorage.getItem("player2");
-    draws.textContent = sessionStorage.getItem("draws");
+// Define The Type For Player1 --> X and Player2 --> O
+let player1 = {
+    symbol: "<i class = 'fa fa-close'></i>",
+    played: [],
+    score: 0,
+}
+let player2 = {
+    symbol: "<i class='fa-sharp fa-regular fa-circle'></i>",
+    played: [],
+    score: 0,
 }
 
-
-// Reset Function
-function reset() {
-    celles.forEach(function(ele) {
-        ele.textContent = "";
-        ele.classList.remove("pick");
+for (let i = 0; i < 9; i++) {
+    cells[i].addEventListener("click", function(ele) {
+        if (!usedCells.includes(i))  {
+            if (turn) {
+                    addSymbol(player1, i); turn = false;
+                    player1.played.push(i);
+                    checkWin(player1);
+                    usedCells.push(i);
+            } else {
+                
+                    addSymbol(player2, i); turn = true;
+                    player2.played.push(i);
+                    checkWin(player2);
+                    usedCells.push(i);
+            }
+        }
     });
-    // Change i value
-    i = 0;
-    // Change The Color Of "O" and "X"
-    o_symbol.style.color = "black";
-    x_symbol.style.color = "white";
-    // remove "shadow" class from content
-    content.classList.remove("shadow");
 }
 
-// Check If The All Box Full Of [X || O]
-function check_all_ele_full() {
-    var check = true; // Supose is Full
-    celles.forEach(function(ele) {
-        if (!ele.classList.contains("pick")) { check = false; return check; }; 
-    });
-    return check;
+// Symbol
+function addSymbol(player, i) {
+    cells[i].innerHTML = player.symbol;
 }
 
 // Check The Winner
-function checkWinner(matrix) {
-    // Check rows
-    for (let i = 0; i < 3; i++) {
-        if (matrix[i][0] === matrix[i][1] && matrix[i][1] === matrix[i][2] && matrix[i][0] !== '-') {
-            return matrix[i][0];
-        }
-    }
-
-    // Check columns
-    for (let j = 0; j < 3; j++) {
-        if (matrix[0][j] === matrix[1][j] && matrix[1][j] === matrix[2][j] && matrix[0][j] !== '-') {
-            return matrix[0][j];
-        }
-    }
-
-    // Check diagonals
-    if ((matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2] && matrix[1][1] !== '-') ||
-        (matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0] && matrix[1][1] !== '-')) {
-        return matrix[1][1];
-    }
-    
-    // No winner
-    return '-';
-}
-
-
-// Check The Winner --> X : Player1, O : Player2 
-function is_there_a_winner() {
-    // Make Matrix That Contain Just "X" and "O"
-    var matrix = [], count = 1, temp = [];
-    celles.forEach(function(ele) {
-        temp[count - 1] = ele.textContent;
-        if (count == 3) { matrix.push(temp); count = 0; temp = []; }
-        count++;
-        console.log(matrix);
-    });
-    console.log(matrix);
-    // Check The Winner and Set The Winner textcontent
-    var check = checkWinner(matrix);
-    return check;
-}
-
-// Fucntion
-function set_result(check) {
-    winner.textContent = check;
-    if (check == "X") {
-        player1.textContent = Number(player1.textContent) + 1;
-        window.sessionStorage.setItem("player1", player1.textContent);
-    }
-    if (check == "0") {
-        player2.textContent = Number(player2.textContent) + 1;
-        window.sessionStorage.setItem("player2", player2.textContent);
-        
-    }
-    if (!(check == "X" && check == "0")) {
-        if (check_all_ele_full()) {
-            draws.textContent = Number(draws.textContent) + 1;
-            window.sessionStorage.setItem("draws", draws.textContent);
-        }
-    }
-}
-
-// The Winner is : ....
-function winner_is() {
-    var check = is_there_a_winner();
-    var condition = (check == "X" || check == "0");
-    if (condition) {
-        toolge.style.display = "flex";
-        content.classList.add("shadow");
-        set_result(check);
-    }
-    if(check_all_ele_full() && (condition)) {
-        toolge.style.display = "flex";
-        content.classList.add("shadow");
-        set_result(check);
-    }
-    if (!condition && check_all_ele_full()) {
-        toolge.style.display = "flex";
-        content.classList.add("shadow");
-        check = "NO WINNER";
-        set_result(check);
-    }
-    // Set The Player1 and Player2
-}
-
-// Load The Page
-window.addEventListener("DOMContentLoaded", reset);
-
-
-// Change X To 0 and 0 To X
-celles.forEach(function(ele) {
-    ele.addEventListener("click", function(e) {
-        // Check The Target Element + add "pick" class
-        if (!(e.target.classList.contains("pick")) && e.target.classList.contains("cell")) {
-            if (i % 2 == 0) {
-                // Change To "X"
-                e.target.textContent = "X";
-                e.target.classList.add("pick");
-                // Change The Content
-                current_trun.textContent = "0";
-                // Change The Color Of "O" and "X"
-                o_symbol.style.color = "white";
-                x_symbol.style.color = "black";
-                winner_is();
-            }
-            else {
-                // Change To "0"
-                e.target.textContent = "0";
-                e.target.classList.add("pick");
-                // Change The Content
-                current_trun.textContent = "X";
-                // Change The Color Of "O" and "X"
-                o_symbol.style.color = "black";
-                x_symbol.style.color = "white";
-                winner_is();
-            }
-            i++;
+function checkWin(player) {
+    var check = winCombos.some(function(combo) {
+        if (combo.every((ele) => player.played.includes(ele))) {
+            reset();
         }
     });
+}
+
+// Reset All Element 
+function reset() {
+    cells.forEach(ele => ele.innerHTML = "");
+    usedCells = [];
+}
+
+// reset button
+reset_btn.addEventListener("click", function() {
+    reset();
 });
 
 
 
-// Reset The Elements
-reset_btn.addEventListener("click", reset);
 
-// Mark And Reset
-mark.addEventListener("click", function() {
-    toolge.style.display = "none";
-    reset();
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Getting The Varibales
+// var celles = document.querySelectorAll(".cell");
+// var current_trun = document.querySelector(".turn .current-trun");
+// var reset_btn = document.querySelector(".reset");
+// var x_symbol = document.querySelector(".x");
+// var o_symbol = document.querySelector(".o");
+// var player1 = document.querySelector(".player1 span");
+// var player2 = document.querySelector(".player2 span");
+// var draws = document.querySelector(".draws span");
+// var toolge = document.querySelector(".toogle");
+// var mark = document.querySelector(".toogle .mark");
+// var winner = document.querySelector(".winner");
+// var content = document.querySelector(".content");
+// var i = 0;
+
+// // Set The Winner And Loser Results
+// if (sessionStorage.getItem("player1") && sessionStorage.getItem("player2") && sessionStorage.getItem("draws")) {
+//     player1.textContent = sessionStorage.getItem("player1");
+//     player2.textContent = sessionStorage.getItem("player2");
+//     draws.textContent = sessionStorage.getItem("draws");
+// }
+
+
+// // Reset Function
+// function reset() {
+//     celles.forEach(function(ele) {
+//         ele.textContent = "";
+//         ele.classList.remove("pick");
+//     });
+//     // Change i value
+//     i = 0;
+//     // Change The Color Of "O" and "X"
+//     o_symbol.style.color = "black";
+//     x_symbol.style.color = "white";
+//     // remove "shadow" class from content
+//     content.classList.remove("shadow");
+// }
+
+// // Check If The All Box Full Of [X || O]
+// function check_all_ele_full() {
+//     var check = true; // Supose is Full
+//     celles.forEach(function(ele) {
+//         if (!ele.classList.contains("pick")) { check = false; return check; }; 
+//     });
+//     return check;
+// }
+
+// // Check The Winner
+// function checkWinner(matrix) {
+//     // Check rows
+//     for (let i = 0; i < 3; i++) {
+//         if (matrix[i][0] === matrix[i][1] && matrix[i][1] === matrix[i][2] && matrix[i][0] !== '-') {
+//             return matrix[i][0];
+//         }
+//     }
+
+//     // Check columns
+//     for (let j = 0; j < 3; j++) {
+//         if (matrix[0][j] === matrix[1][j] && matrix[1][j] === matrix[2][j] && matrix[0][j] !== '-') {
+//             return matrix[0][j];
+//         }
+//     }
+
+//     // Check diagonals
+//     if ((matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2] && matrix[1][1] !== '-') ||
+//         (matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0] && matrix[1][1] !== '-')) {
+//         return matrix[1][1];
+//     }
+    
+//     // No winner
+//     return '-';
+// }
+
+
+// // Check The Winner --> X : Player1, O : Player2 
+// function is_there_a_winner() {
+//     // Make Matrix That Contain Just "X" and "O"
+//     var matrix = [], count = 1, temp = [];
+//     celles.forEach(function(ele) {
+//         temp[count - 1] = ele.textContent;
+//         if (count == 3) { matrix.push(temp); count = 0; temp = []; }
+//         count++;
+//         console.log(matrix);
+//     });
+//     console.log(matrix);
+//     // Check The Winner and Set The Winner textcontent
+//     var check = checkWinner(matrix);
+//     return check;
+// }
+
+// // Fucntion
+// function set_result(check) {
+//     winner.textContent = check;
+//     if (check == "X") {
+//         player1.textContent = Number(player1.textContent) + 1;
+//         window.sessionStorage.setItem("player1", player1.textContent);
+//     }
+//     if (check == "0") {
+//         player2.textContent = Number(player2.textContent) + 1;
+//         window.sessionStorage.setItem("player2", player2.textContent);
+        
+//     }
+//     if (!(check == "X" && check == "0")) {
+//         if (check_all_ele_full()) {
+//             draws.textContent = Number(draws.textContent) + 1;
+//             window.sessionStorage.setItem("draws", draws.textContent);
+//         }
+//     }
+// }
+
+// // The Winner is : ....
+// function winner_is() {
+//     var check = is_there_a_winner();
+//     var condition = (check == "X" || check == "0");
+//     if (condition) {
+//         toolge.style.display = "flex";
+//         content.classList.add("shadow");
+//         set_result(check);
+//     }
+//     if(check_all_ele_full() && (condition)) {
+//         toolge.style.display = "flex";
+//         content.classList.add("shadow");
+//         set_result(check);
+//     }
+//     if (!condition && check_all_ele_full()) {
+//         toolge.style.display = "flex";
+//         content.classList.add("shadow");
+//         check = "NO WINNER";
+//         set_result(check);
+//     }
+//     // Set The Player1 and Player2
+// }
+
+// // Load The Page
+// window.addEventListener("DOMContentLoaded", reset);
+
+
+// // Change X To 0 and 0 To X
+// celles.forEach(function(ele) {
+//     ele.addEventListener("click", function(e) {
+//         // Check The Target Element + add "pick" class
+//         if (!(e.target.classList.contains("pick")) && e.target.classList.contains("cell")) {
+//             if (i % 2 == 0) {
+//                 // Change To "X"
+//                 e.target.textContent = "X";
+//                 e.target.classList.add("pick");
+//                 // Change The Content
+//                 current_trun.textContent = "0";
+//                 // Change The Color Of "O" and "X"
+//                 o_symbol.style.color = "white";
+//                 x_symbol.style.color = "black";
+//                 winner_is();
+//             }
+//             else {
+//                 // Change To "0"
+//                 e.target.textContent = "0";
+//                 e.target.classList.add("pick");
+//                 // Change The Content
+//                 current_trun.textContent = "X";
+//                 // Change The Color Of "O" and "X"
+//                 o_symbol.style.color = "black";
+//                 x_symbol.style.color = "white";
+//                 winner_is();
+//             }
+//             i++;
+//         }
+//     });
+// });
+
+
+
+// // Reset The Elements
+// reset_btn.addEventListener("click", reset);
+
+// // Mark And Reset
+// mark.addEventListener("click", function() {
+//     toolge.style.display = "none";
+//     reset();
+// });
 
 
 
