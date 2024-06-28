ef44f248729f1074d47b6712ba217ecea621b6aa
Abdellah <AbdellahKarani>
2024-06-28 22:00:00 +0100

    .

---
+++ project 18/main.js
@@ -0,0 +1,19 @@
+// The Project Will Be Generate A Password And Copy The Password That Generated
+
+// Getting Varibles
+let input = document.querySelector(".generate-inputs input");
+let copy = document.querySelector(".generate-inputs button");
+let generate = document.querySelector("generate-pass button");
+
+
+// generate Password
+function generate_password() {
+
+}
+
+
+
+
+
+
+
