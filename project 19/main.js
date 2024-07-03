c94ae3e1f07f28f8b7a4f5cf3775064344015ead
Abdellah <AbdellahKarani>
2024-07-03 18:20:32 +0100

    .

--- project 19/main.js
+++ project 19/main.js
@@ -1,5 +1,17 @@
 const toggle = document.querySelector(".mode .toggle");
 const circle = document.querySelector(".mode .toggle .circle");
 
-// custom the color and fonts icons and toggle
+toggle.addEventListener("click", function() {
+    toggle.classList.toggle("dark");
+    circle.classList.toggle("dark");
+    // Here Exit The Changes
+});
+
+// Search Video About Custom The Toggle
+// Also About Multiples Color
+
+
+
+
+
 
