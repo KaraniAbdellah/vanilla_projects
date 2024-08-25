5413042c2e0e2f805d309c1f8571cf11feddc97d
Abdellah <AbdellahKarani>
2024-08-25 17:53:39 +0100

    .

--- project 26/js/main.js
+++ project 26/js/main.js
@@ -1,7 +1,52 @@
 // do some thing
 
 
+// Profile
+var url = "https://api.github.com/users/KaraniAbdellah";
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", url);
+myRequest.send();
+myRequest.onload = function() {
+    if (myRequest.status == 200 && myRequest.readyState == 4) {
+        let Data = JSON.parse(myRequest.responseText);
+        // bio
+        console.log(Data.location);
+        console.log(Data.name);
+        console.log(Data.public_repos);
+        console.log(Data.bio);
+        console.log(Data.blog);
+        console.log(Data.repos_url);
+        console.log(Data.avatar_url);
+        // // projects
+        // url = url + '/repos';
+        // console.log(Data[0].name);
+        // console.log(Data[0].clone_url);
+        // console.log(Data[0].visibility);
+        // console.log(Data[0].watchers_count);
+        // console.log(Data[0].ssh_url);
+        // console.log(Data[0].clone_url);
+        // console.log(Data[0]);
+    }
+}
 
-console.log("Hello");
 
+// projects
+var repos_url = url + "/repos";
+var myRequest2 = new XMLHttpRequest();
+myRequest2.open("GET", repos_url);
+myRequest2.send();
+myRequest2.onload = function() {
+    if (myRequest2.status == 200 && myRequest2.readyState == 4) {
+        let Data = JSON.parse(myRequest2.responseText);
+        // projects
+        url = url + '/repos';
+        console.log(Data[0].name);
+        console.log(Data[0].clone_url);
+        console.log(Data[0].visibility);
+        console.log(Data[0].watchers_count);
+        console.log(Data[0].ssh_url);
+        console.log(Data[0].clone_url);
+        console.log(Data[0]);
+    }
+}
 
