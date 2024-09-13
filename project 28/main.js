4f5a189b0e57e4c9cee2ec6c0a90ee3cbc3393d4
Abdellah <AbdellahKarani>
2024-09-13 19:39:14 +0100

    .

--- project 28/main.js
+++ project 28/main.js
@@ -1,28 +1,44 @@
 const input_data = document.querySelector("input");
 const button_dwn = document.querySelector("button");
+const error_ele = document.querySelector(".error");
 
 button_dwn.addEventListener("click", function() {
     if (input_data.value) {
-         
-        fetch(input_data.value, {mode: 'no-cros'})
-        .then((reponse) => {
-            return reponse.blob();
-        }).then((blob) => {
-            console.log(blob.type.replace("/", "."));
-            const myUrl = URL.createObjectURL(blob);
-            const aTag = document.createElement("a");
-            aTag.href = myUrl;
-            aTag.download = blob.type.replace("/", ".");
-            document.body.appendChild(aTag);
-            aTag.click();
-            aTag.remove();
-            // 
-        }).catch((msg) => {
-            console.log(msg);
-        });
+        try {
+            fetch(input_data.value)
+            .then((reponse) => {
+                if (!reponse.ok) showError();
+                return reponse.blob();
+            }).then((blob) => {
+                download(blob);
+            }).catch((msg) => {
+                showError();
+            });
+        } catch (error) {
+            showError();
+        }
 
     } else input_data.focus();
-})
+});
 
+
+function download(blob) {
+    const myUrl = URL.createObjectURL(blob);
+    const aTag = document.createElement("a");
+    aTag.href = myUrl;
+    aTag.download = blob.type.replace("/", ".");
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+}
+
+
+
+function showError() {
+    error_ele.classList.remove("hidden");
+    setTimeout(() => {
+        error_ele.classList.add("hidden");
+    }, 2000);
+}
 
 
