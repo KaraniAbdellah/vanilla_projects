56025b11c609cfcc58ca2160a47970860862e054
abdellah <abdellahkarani@gmail.com>
2024-09-15 19:34:46 +0100

    .

---
+++ project 29/main.js
@@ -0,0 +1,29 @@
+const btn_name = document.querySelectorAll("button");
+
+btn_name.forEach(element => {
+    element.addEventListener("click", function(e) {
+        console.log(e.target.className);
+        const div_ele = document.querySelector(`div.${e.target.className}`);
+        console.log(div_ele);
+        div_ele.classList.add("show");
+        // setTimeout(() => {
+        //     div_ele.classList.remove("show");
+        // }, 1000);
+    });
+});
+
+const close_icons = document.querySelectorAll(".close");
+close_icons.forEach(element => {
+    element.addEventListener("click", function() {
+        const div_ele = element.parentElement;
+        div_ele.classList.remove("show");
+    });
+});
+
+
+
+
+
+
+
+
